9a7cc64999fadc712ef915a7cebfde677aacd5c8
Gregoire Dechaine <gregdechaine@me.com>
2024-08-11 13:45:56 +0000

    Bug Fix: Update aps-redirect-to-auth w correct env variable

--- vue-app/netlify/functions/aps-redirect-to-auth.mjs
+++ vue-app/netlify/functions/aps-redirect-to-auth.mjs
@@ -1,7 +1,7 @@
 // netlify/functions/aps-redirect-to-auth.js
 exports.handler = async function(event, context) {
     const clientId = process.env.APS_CLIENT_ID;
-    const redirectUri = process.env.REDIRECT_URI;
+    const redirectUri = process.env.APS_REDIRECT_URI;
   
     // Redirect the user to Autodesk Platform Service's (APS) authentication page
     return {
